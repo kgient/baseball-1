318e310b118cfffc272d804a07d713399f4e64d8
ksh <ksh@samsung>
2024-07-10 14:49:50 +0900

    [feature] 39

--- game.py
+++ game.py
@@ -3,5 +3,9 @@ class Game:
         if guessNumber is None:
             raise TypeError()
 
-        if len(guessNumber) !=3:
+        if len(guessNumber) != 3:
             raise TypeError()
+
+        for number in guessNumber:
+            if not ord('0') <= ord(number) <= ord('9'):
+                raise TypeError()
